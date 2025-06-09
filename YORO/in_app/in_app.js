5013515cf13f929579552a918be3c80e6530c747
Talaat <talaatgamal.dev@gmail.com>
2025-06-09 06:43:29 +0300

    k

--- YORO/in_app/in_app.js
+++ YORO/in_app/in_app.js
@@ -62,25 +62,9 @@ const elements = document.querySelectorAll('.ad-advanced');
 
 elements.forEach(function(element) {
   element.addEventListener('click', function() {
-    
-    // نقرأ قيمة aria-details من نفس العنصر اللي اتضغط عليه
-    const appName = element.getAttribute("aria-details");
 
-    // نعرض القيمة داخل العنصر اللي id بتاعه app_name
-    const displayElement = document.getElementById("app_name");
-    if (displayElement) {
-      displayElement.textContent = appName;
-    }
-
-    // نفتح نافذة in-app مع تأثير
-    in_app.style.display = "flex";
-    
-    setTimeout(() => {
-      in_app.style.opacity = "1";
-      in_app.style.width = "95vw";
-      in_app.style.height = "85vh";
-    }, 1);
-  });
+          document.getElementById("aa-2").style.height="240px"
+  })
 });
 
 
@@ -91,7 +75,12 @@ let exit_window = document.getElementById("exit-window")
 
 exit_window.addEventListener('click', function(){
     
+
     in_app.style.opacity = "0";
       in_app.style.width = "0vw";
       in_app.style.height = "0vh";
 })
+
+
+
+
